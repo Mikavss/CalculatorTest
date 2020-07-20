2ffb328413354bc5b9b6a25a1f10b505ba03d705
Шмакова Вероника Витальевна <66990004+Mikavss@users.noreply.github.com>
2020-07-20 09:12:47 +0400

    Изменён калькулятор

--- src/main/java/ru/lanit/testng/Main.java
+++ src/main/java/ru/lanit/testng/Main.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите номер необходимого действия: \n1 - Сложение \n2 - Вычитание \n3 - Деление \n4 - умножение");
-        int Calculator = scanner.nextInt();
+        System.out.println("Введите необходимое действие (+,-,/,*)");
+        String Calculator = scanner.nextLine();
         System.out.println("Введите первое число: ");
         double x = scanner.nextDouble();
         System.out.println("Введите второе число: ");
         double y = scanner.nextDouble();
 
         switch (Calculator) {
-            case 1:
+            case "+":
                 System.out.println("Результат: " + Math.sumTest(x, y));
                 break;
-            case 2:
+            case "-":
                 System.out.println("Результат: " + Math.diffTest(x, y));
                 break;
-            case 3: {
+            case "/": {
                 if (y == 0) {
                     System.out.println("Нельзя делить на 0");
                 } else {
@@ -27,7 +27,7 @@ public class Main {
                 }
                 break;
             }
-            case 4:
+            case "*":
                 System.out.println("Результат: " + Math.multTest(x, y));
                 break;
             default:
